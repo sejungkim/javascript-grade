529f6ef9d73bc0d91d65b14245e73127daf5d5db
hannarita <harugio@naver.com>
2017-11-16 16:13:40 +0900

    add: calculateMajor()

--- index.js
+++ index.js
@@ -2,32 +2,38 @@ var data =  [
     {
         'name' : '데이터베이스',
         'grade' : 'A',
-        'credit' : 3
+        'credit' : 3,
+        'major' : true
     },
     {
         'name' : '교양영어',
         'grade' : 'B+',
-        'credit' : 2
+        'credit' : 2,
+        'major' : false
     },
     {
         'name' : '철학',
         'grade' : 'B+',
-        'credit' : 1
+        'credit' : 1,
+        'major' : false
     },
     {
         'name' : '미디어아트개론',
         'grade' : 'A+',
-        'credit' : 3
+        'credit' : 3,
+        'major' : true
     },
     {
         'name' : '디자인론',
         'grade' : 'B+',
-        'credit' : 3
+        'credit' : 3,
+        'major' : true
     },
     {
         'name' : '그래픽디자인및실습',
         'grade' : 'C+',
-        'credit' : 3
+        'credit' : 3,
+        'major' : true
     }
 
 ];
@@ -65,6 +71,28 @@ function calculateGpa() {
     }
 
     var result = (gpMultiple / gpSum).toFixed(2);
+    var majorResult = calculateMajor();
+
     console.log('총평점:'+ result);
+    console.log('전공평점:'+ majorResult);
     console.log('이수학점:'+ gpSum);
+
+
 }calculateGpa();
+
+
+function calculateMajor() {
+    var gpMultiple = 0;
+    var gpSum = 0;
+
+    for(var i=0; i<data.length; i++){
+        if(data[i].major){
+            gpMultiple += (data[i].credit) * (data[i].grade);
+            gpSum += data[i].credit;
+        }
+
+        var majorResult = (gpMultiple / gpSum).toFixed(2);
+    }
+
+    return majorResult;
+}
