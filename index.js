4be6d523f8fb4d6134012a4eff1bb68cdc0b284b
hannarita <harugio@naver.com>
2017-11-28 10:36:25 +0900

    ref: add node readline

--- index.js
+++ index.js
@@ -1,115 +1,60 @@
-var data =  [
-    {
-        'name' : '데이터베이스',
-        'grade' : 'A',
-        'credit' : 3,
-        'major' : true
-    },
-    {
-        'name' : '교양영어',
-        'grade' : 'B+',
-        'credit' : 2,
-        'major' : false
-    },
-    {
-        'name' : '철학',
-        'grade' : 'B+',
-        'credit' : 1,
-        'major' : false
-    },
-    {
-        'name' : '미디어아트개론',
-        'grade' : 'A+',
-        'credit' : 3,
-        'major' : true
-    },
-    {
-        'name' : '디자인론',
-        'grade' : 'B+',
-        'credit' : 3,
-        'major' : true
-    },
-    {
-        'name' : '그래픽디자인및실습',
-        'grade' : 'C+',
-        'credit' : 3,
-        'major' : true
+const readline = require('readline');
+
+var data = [];
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function convertGrade(currGrade) {
+    var grade = 'FDCBA';
+    var score = grade.indexOf(currGrade[0]);
+    if(grade[1] === '+' && score !== 0){
+        score += 0.5;
     }
-
-];
-
-var grades= [];
-var creditSum = 0;
-
-function calculateGpa(percentile) {
-    grades = [];
-    convertGrade(percentile);
-
-    var gpMultiple = 0;
-    var gpSum = 0;
-
-    for(var i=0; i<data.length; i++) {
-        gpMultiple += (data[i].credit) * (grades[i]);
-        gpSum += data[i].credit;
-    }
-
-    creditSum = gpSum;
-    var res = (gpMultiple / gpSum).toFixed(2);
-
-    return res;
+    return score;
 }
 
-function convertGrade(percentile) {
-    for(var i=0; i<data.length; i++){
-        var grade = data[i].grade;
-        switch(grade){
-            case 'A+': grades.push(percentile);
-                break;
-            case 'A': grades.push(percentile-0.5);
-                break;
-            case 'B+': grades.push(percentile-1);
-                break;
-            case 'B': grades.push(percentile-1.5);
-                break;
-            case 'C+': grades.push(percentile-2.0);
-                break;
-            case 'C': grades.push(percentile-2.5);
-                break;
-            case 'F': grades.push(0);
-                break;
+function calculateGradepoint(data) {
+    var creditSum = 0 , creditGradeSum = 0;
+    var majorCreditSum = 0 , majorCreditGradeSum = 0;
+    var gpa = 0,majorGpa = 0;
+    var convertGpa = 0;
+
+    data.forEach(function (curr) {
+        creditSum += curr.credit;
+        creditGradeSum += convertGrade(curr.grade)*curr.credit;
+        if(curr.bMajor){
+            if(curr.bMajor)
+            majorCreditSum += convertGrade(curr.grade);
+            majorCreditGradeSum += convertGrade(curr.grade)*curr.credit;
         }
-    }
+    });
+
+    gpa = (creditGradeSum/creditSum).toFixed(2);
+    majorGpa = (majorCreditGradeSum/majorCreditSum).toFixed(2);
+    convertGpa = ((creditGradeSum/creditSum) * 4 / 4.5).toFixed(2);
+
+    console.log('총평점 : ' + gpa);
+    console.log('전공평점 : ' + majorGpa);
+    console.log('이수학점 : ' + creditSum);
+    console.log('전공이수학점 : ' + majorCreditSum);
+    console.log('4.0으로 변환시 : ' + convertGpa);
+
 }
 
-function calculateMajor() {
-    var gpMultiple = 0;
-    var gpSum = 0;
-
-    for(var i=0; i<data.length; i++){
-        if(data[i].major){
-            gpMultiple += (data[i].credit) * grades[i];
-            gpSum += data[i].credit;
+function addLecture() {
+    rl.question('과목을 JSON형태로 입력하세요<종료는 end입력>',function(answer){
+        if(answer === 'end'){
+            setTimeout(function () {
+                calculateGradepoint(data);
+            },2000);
+            return rl.close();
         }
-    }
-    var majorResult = (gpMultiple / gpSum).toFixed(2);
-    return majorResult;
-}
 
-function addLecture(lecture) {
-    data.push(lecture);
-    showResult();
-}
+        data.push(JSON.parse(answer));
+        addLecture();
+    });
 
-function showResult() {
-    var result = calculateGpa(4.5);
-    var majorResult = calculateMajor();
-    var convertResult = calculateGpa(4.0);
-
-    console.log('총평점:'+ result);
-    console.log('전공평점:'+ majorResult);
-    console.log('이수학점:'+ creditSum);
-    console.log('4.0 학점으로 변환하는 경우 총평점은: '+ convertResult);
-}
-
-showResult();
-addLecture({'name' : '알고리즘', 'grade' : 'B', 'credit' : 3, 'bMajor' : true});
+}addLecture();
